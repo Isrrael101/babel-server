1ee714ae75f2be7da99f3961bb49db5363b7345a
Isrrael101 <isrraelfcaq@hotmail.com>
2021-12-16 22:05:59 -0400

    ya estamos listos

--- dist/index.js
+++ dist/index.js
@@ -9,10 +9,13 @@ var _http = _interopRequireDefault(require("http"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+const PORT = process.env.PORT || 4000;
+
 const server = _http.default.createServer((req, res) => {
   res.end("Hello from the server");
-}).listen(4001);
+}).listen(PORT, function () {
+  console.log("Servidor conectado en el puerto", PORT);
+});
 
-console.log("Server is up and running");
 var _default = server;
 exports.default = _default;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import http from "http";
 
+const PORT = process.env.PORT || 4000;
+
 const server = http
   .createServer((req, res) => {
     res.end("Hello from the server");
   })
-  .listen(4001);
-
-console.log("Server is up and running");
+  .listen(PORT, function() {
+  console.log("Servidor conectado en el puerto", PORT)
+});
 
 export default server;
